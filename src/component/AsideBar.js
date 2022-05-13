e203e8a22f3324d7e10a8c812c7502277483bc79
58peiShan <99083443+58peiShan@users.noreply.github.com>
2022-05-13 15:42:44 +0800

    Update AsideBar.js

--- src/component/AsideBar.js
+++ src/component/AsideBar.js
@@ -7,7 +7,7 @@ function AsideBar(){
         <input type="checkbox" name="" id="sideMenu-switch"></input>
             <aside className="d-flex flex-column align-items-center pb-5 px-3 asideBar">
               <label for="sideMenu-switch" className="align-self-end">
-   	          <FaAngleLeft className='faAngleLeft'/></label>
+   	          <FaAngleRight className='faAngleRight'/></label>
               <button className='btn btn-primary mt-5 mb-4 rounded-pill'>{"新增討論"}</button>
               <button className='btn asideBtn'>{"逛展攻略"}</button>
               <button className='btn asideBtn'>{"逛展攻略"}</button>
