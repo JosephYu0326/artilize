a4de3aa091725605d52d5ac54d09e9c129f55890
58peiShan <99083443+58peiShan@users.noreply.github.com>
2022-05-15 14:08:14 +0800

    Update AsideBar.js

--- src/component/AsideBar.js
+++ src/component/AsideBar.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {FaStar,FaAngleLeft, FaAngleRight, FaCommentDots } from 'react-icons/fa'
+import {FaStar,FaAngleLeft } from 'react-icons/fa'
 
 function AsideBar(){
     return(
         <>
         <input type="checkbox" name="" id="sideMenu-switch"></input>
-            <aside className="d-flex flex-column align-items-center pb-5 px-3 asideBar">
+            <aside className="pb-5 px-3 asideBar">
               <label for="sideMenu-switch" className="align-self-end">
-   	          <FaAngleRight className='faAngleRight'/></label>
-              <button className='btn btn-primary mt-5 mb-4 rounded-pill'>{"新增討論"}</button>
+   	          <FaAngleLeft className='faAngleLeft'/></label>
+              <button className='btn btn-primary mt-5 mb-4 py-2 px-3 rounded-pill'>{"新增討論"}</button>
               <button className='btn asideBtn'>{"逛展攻略"}</button>
               <button className='btn asideBtn'>{"逛展攻略"}</button>
               <button className='btn asideBtn'>{"逛展攻略"}</button>
