cb25137f16fa20426f427a4823c5504ee6cfc53c
JosephYu0326 <pkujm951@gmail.com>
2022-05-17 13:12:21 +0800

    首頁更新

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,6 +13,7 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 import 'swiper/css/autoplay'
+import CountUp from 'react-countup'
 
 function Home(props) {
   function rand(min, max) {
@@ -126,9 +127,58 @@ function Home(props) {
         </Container>
       </section>
       <section>
-        <Container fluid>
+        <Container fluid className="homeMap">
           <Row>
-            <div className="homeMap">1</div>
+            <div>
+              <Container className="homeMapInside d-flex flex-column  justify-content-center">
+                <div className="d-flex align-items-center">
+                  <CountUp start={0} end={1689} delay={0} duration={5}>
+                    {({ countUpRef }) => (
+                      <h1 className="ExtraBold">
+                        <span ref={countUpRef} />
+                      </h1>
+                    )}
+                  </CountUp>
+                  <h5 className="ExtraBold ">
+                    場展覽在此舉
+                    <br />行
+                  </h5>
+                </div>
+                <div className="d-flex homeMapButton justify-content-between ">
+                  <button className=" btn btn-primary">
+                    <h2>關鍵字搜尋</h2>
+                  </button>
+                  <button className=" btn ">
+                    <h2>地圖搜尋</h2>
+                  </button>
+                </div>
+                <div className="homeMapSearchBar d-flex  align-items-stretch">
+                  <form className="d-flex align-items-center justify-content-center ">
+                    <input type="search" placeholder="" />
+                    <button type="submit">Search</button>
+                  </form>
+                </div>
+              </Container>
+            </div>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container fluid className="homeRank">
+          <Row>
+            <div>
+              <Container>
+                <div className="homeRankHeader">
+                  <h1 className="SemiBold">
+                    <span>展覽活動排行榜</span>
+                  </h1>
+                </div>
+                <h6>此時此刻最熱門的展覽，活動與藝術家介紹</h6>
+                <button className="btn d-flex justify-content-between">
+                  <h6>更多展覽活動</h6>
+                </button>
+              </Container>
+            </div>
           </Row>
         </Container>
       </section>
