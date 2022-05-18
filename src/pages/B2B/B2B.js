0b09b6c9f0839d76f0b4165751b098e05c66d1c6
孤鴻寄語默蒼離 <101337985+Chuan-OWO@users.noreply.github.com>
2022-05-18 16:19:07 +0800

    切版

--- src/pages/B2B/B2B.js
+++ src/pages/B2B/B2B.js
@@ -1,13 +1,292 @@
 //廠商後臺
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../../styles/B2B.scss'
+// import { useState } from 'react'
 
 function B2B(props) {
+  // const [inputs, setInputs] = useState({})
+
+  // const handleChange = (event) => {
+  //   const name = event.target.name
+  //   const value = event.target.value
+  //   setInputs((values) => ({ ...values, [name]: value }))
+  // }
+
   return (
     <>
+      {/*  */}
       <div>廠商後臺</div>
-      <Link to="/b2b/addability">建立新活動資料aaaaaaaaaa</Link>
-      <br />
+      <Link to="/b2b/addability">建立新活動資料</Link>
+      <main className="container ">
+        <div className="row d-flex justify-content-center ">
+          {/*onSubmit=handleSubmit*/}
+          <form className="col-6 b2bForm ">
+            <h1>開始建立活動資訊</h1>
+            <h6>請創建一個活動展覽資訊</h6>
+
+            <figure className="figure d-flex justify-content-center ">
+              <img
+                src="../images/logo.svg"
+                className="figure-img img-fluid rounded"
+                alt="..."
+              />
+            </figure>
+
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                活動名稱
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="活動名稱"
+              />
+            </div>
+
+            <div>請輸入活動名稱</div>
+            <h3>活動時間</h3>
+            <section>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  開始時間
+                </label>
+                <input
+                  type="datetime-local"
+                  className="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="活動時間"
+                />
+              </div>
+              <h6>請輸入活動時間</h6>
+            </section>
+
+            <section>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  結束時間
+                </label>
+                <input
+                  type="datetime-local"
+                  className="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="活動時間"
+                />
+              </div>
+              <h6>請輸入活動時間</h6>
+            </section>
+
+            <section>
+              <label htmlFor="exampleDataList" className="form-label">
+                活動地點
+              </label>
+              <select className="form-select" id="sel1" name="Place">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </section>
+
+            <h6>請選擇縣市</h6>
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                詳細地址（鄉鎮市區、道路、街名、巷弄號、樓層）
+              </label>
+              <input
+                type="value"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="例如:中山區中山北路二段106-2號6樓"
+              />
+            </div>
+            <h6>請輸入詳細活動地址</h6>
+            {/* 活動內容介紹 */}
+            <div className="mb-3">
+              <label
+                htmlFor="exampleFormControlTextarea1"
+                className="form-label"
+              >
+                活動內容介紹
+              </label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="10"
+                value={``}
+              >
+                活動內容
+              </textarea>
+            </div>
+
+            <section>
+              <div className="row justify-content-center">
+                <h5>新增活動票券</h5>
+                <div className="col-3 ">
+                  <div className="mb-3">
+                    <label
+                      htmlFor="exampleFormControlInput1"
+                      className="form-label"
+                    >
+                      票券名稱
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="exampleFormControlInput1"
+                      placeholder="活動名稱"
+                    />
+                  </div>
+                </div>
+                <div className="col-3 ">
+                  <div className="mb-3">
+                    <label
+                      htmlFor="exampleFormControlInput1"
+                      className="form-label"
+                    >
+                      票券數量
+                    </label>
+                    <input
+                      type="value"
+                      className="form-control"
+                      id="exampleFormControlInput1"
+                      placeholder="1000"
+                    />
+                  </div>
+                </div>
+                <div className="col-3 ">
+                  <div className="mb-3">
+                    <label
+                      htmlFor="exampleFormControlInput1"
+                      className="form-label"
+                    >
+                      票券價格
+                    </label>
+                    <input
+                      type="value"
+                      className="form-control"
+                      id="exampleFormControlInput1"
+                      placeholder="1000"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-3 ">
+                  <div className="mb-3">
+                    <label
+                      htmlFor="exampleFormControlInput1"
+                      className="form-label"
+                    >
+                      操作
+                    </label>
+                    <input
+                      type="value"
+                      className="form-control"
+                      id="exampleFormControlInput1"
+                      placeholder="1000"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="exampleFormControlTextarea1"
+                  className="form-label"
+                >
+                  票券說明
+                </label>
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="10"
+                  value={``}
+                >
+                  請輸入票券說明
+                </textarea>
+              </div>
+            </section>
+
+            <section>
+              <section>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    開始時間
+                  </label>
+                  <input
+                    type="datetime-local"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="活動時間"
+                  />
+                </div>
+                <h6>請輸入活動時間</h6>
+              </section>
+
+              <section>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    開始時間
+                  </label>
+                  <input
+                    type="datetime-local"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="活動時間"
+                  />
+                </div>
+                <h6>請輸入活動時間</h6>
+              </section>
+            </section>
+
+            <section>
+              <div>
+                <div className="d-grid gap-3">
+                  <button type="button" className="btn btn-primary my-2">
+                    收合資訊
+                  </button>
+                </div>
+              </div>
+
+              <div>
+                <div className="d-grid gap-2 d-md-flex justify-content-md-center">
+                  <button
+                    className="btn btn-primary me-md-5 my-2"
+                    type="button"
+                  >
+                    +免費票
+                  </button>
+                  <button className="btn btn-primary my-2" type="button">
+                    {/* <i className="bi bi-plus-lg"></i> */}
+                    +付費票
+                  </button>
+                </div>
+              </div>
+
+              <div>
+                {/* sumbit */}
+                <div className="d-grid gap-2 col-6 mx-auto my-2">
+                  <button className="btn m btn-primary" type="submit">
+                    確認
+                  </button>
+                </div>
+              </div>
+            </section>
+          </form>
+        </div>
+      </main>
     </>
   )
 }
--- src/pages/Booking/Pay.js
+++ src/pages/Booking/Pay.js
@@ -1,12 +1,204 @@
 //付款
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../../styles/Pay.scss'
 
 function Pay(props) {
   return (
     <>
-      <div>付款</div>
       <Link to="/booking/purchasesuccess">購買完成</Link>
+      <main>
+        <div className="contaniner allstep">
+          <div className="row ">
+            <div className=" d-flex justify-content-center p-0">
+              <div className="col-4 p-0">
+                <div className="textstep1">
+                  <h3>確認商品</h3>
+                </div>
+              </div>
+              <div className="col-4 p-0">
+                <div className="textstep2">
+                  <h3>確認付款</h3>
+                </div>
+              </div>
+              <div className="col-4 p-0">
+                <div className="textstep3">
+                  <h3>購買完成</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ul className="step">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+        <div className="container ">
+          <div className="row d-flex justify-content-center row-cols-1">
+            <div className="col-6">
+              <h1 className="payyes">確認付款</h1>
+            </div>
+          </div>
+        </div>
+        <form>
+          <div className="container ">
+            <div className="row d-flex justify-content-center ">
+              <div className="col-8 col-md-8  ">
+                <section className="payForm pb-4 mb-5">
+                  <h1>付款方式</h1>
+
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    信用卡
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="卡號 0000 0000 0000"
+                  />
+                  <h6>請輸入正確的卡號</h6>
+
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  ></label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="持卡人姓名 CHUNG HSIEN YU"
+                  />
+                  <h6>請輸入正確的姓名</h6>
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  ></label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="有效日期 04/29"
+                  />
+                  <h6>請輸入有效的日期</h6>
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  ></label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="信用卡後三碼"
+                  />
+                  <section>
+                    <h6>請輸入正確性名</h6>
+                  </section>
+                </section>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="row d-flex justify-content-center">
+              <div className="col-8 ">
+                <section className="payForm pb-4 mb-5">
+                  <h3>收件人資料</h3>
+
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  ></label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="姓名 冰川沙耶"
+                  />
+                  <h6>請輸入正確的卡號</h6>
+
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  ></label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="持卡人姓名 CHUNG HSIEN YU"
+                  />
+                  <h6>請輸入正確地址</h6>
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  ></label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="有效日期 04/29"
+                  />
+                  <h6>請輸入正確的email</h6>
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  ></label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="信用卡後三碼"
+                  />
+                  <h6>請輸入正確的電話格式</h6>
+                </section>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="row d-flex justify-content-center">
+              <div className="col-8 ">
+                <div className="d-flex flex-row bd-highlight mb-3 justify-content-between  ">
+                  <div className="p-2 bd-highlight">
+                    <h6>優惠券哲扣</h6>
+                  </div>
+                  <div className="p-2 bd-highlight">
+                    <h6>-$10</h6>
+                  </div>
+                </div>
+                <div className="d-flex flex-row bd-highlight mb-3 justify-content-between  ">
+                  <div className="p-2 bd-highlight">
+                    <h6>總金額</h6>
+                  </div>
+                  <div className="p-2 bd-highlight">
+                    <h6>$190</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="row d-flex justify-content-center">
+              <div className="col-8 ">
+                <section className="payForm pb-4 mb-5">
+                  <div className="d-grid gap-2 ">
+                    <button className="btn btn-primary " type="button">
+                      上一步
+                    </button>
+                    <button className="btn btn-primary" type="button">
+                      送出訂單
+                    </button>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </form>
+      </main>
       <br />
     </>
   )
