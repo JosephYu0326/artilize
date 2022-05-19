9be904f573d620f7bd14dabfa6c3c2f177261b32
孤鴻寄語默蒼離 <101337985+Chuan-OWO@users.noreply.github.com>
2022-05-19 15:12:19 +0800

    新增 購買完成頁面

--- src/pages/Booking/PurchaseSuccess.js
+++ src/pages/Booking/PurchaseSuccess.js
@@ -6,7 +6,32 @@ function PurchaseSuccess(props) {
   return (
     <>
       <div>購買完成</div>
-      <br />
+      <div className="contaniner allTextStep">
+        <div className="row d-flex justify-content-center">
+          <div className="col-4 p-0">
+            <div className="textStep1">
+              <h3 className="ExtraBold">確認商品</h3>
+            </div>
+          </div>
+          <div className="col-4 p-0">
+            <div className="textStep2">
+              <h3 className="ExtraBold">確認付款</h3>
+            </div>
+          </div>
+          <div className="col-4 p-0">
+            <div className="textStep3">
+              <h3 className="ExtraBold">購買完成</h3>
+            </div>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <ul className="step p-0">
+            <li></li>
+            <li classname="stepActive"></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
