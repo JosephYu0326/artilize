e7e6ae1be81374612f1932f95fbddbffb148e5a4
58peiShan <99083443+58peiShan@users.noreply.github.com>
2022-05-19 14:45:44 +0800

    Update Header.js
    Header變漂亮了

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Navbar, Container, Button } from "react-bootstrap";
+import React, { useState} from "react";
+import { Nav, Navbar, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../images/logo.png";
 import { FaUserCircle, FaSearch } from "react-icons/fa";
@@ -8,28 +8,30 @@ import "../styles/HeaderAndFooter.scss";
 import "../styles/SearchBar.scss";
 
 function Header(props) {
-  const [collapse, setCollapse] = useState("navbar-collapse collapse");
+  // const [collapse, setCollapse] = useState("navbar-collapse collapse");
   const [collapseSearch, setCollapseSearch] = useState(
     "navbar-collapse collapse"
   );
-  const [ariaExpanded, setAriaExpanded] = useState("false");
-  const [navbarToggler, setNavbarToggler] = useState(
-    "navbar-toggler collapsed"
-  );
+  // const [ariaExpanded, setAriaExpanded] = useState("false");
+  // const [navbarToggler, setNavbarToggler] = useState(
+  //   "navbar-toggler collapsed"
+  // );
 
-  useEffect(() => {});
+  // function handleClick() {
 
-  function handleClick() {
-    if (collapse === "navbar-collapse collapse") {
-      setCollapse("navbar-collapse collapse show");
-      setAriaExpanded("true");
-      setNavbarToggler("navbar-toggler");
-    } else {
-      setCollapse("navbar-collapse collapse");
-      setAriaExpanded("false");
-      setNavbarToggler("navbar-toggler collapsed");
-    }
-  }
+  //   if (collapse === "navbar-collapse collapse") {
+  //     setCollapse("navbar-collapse collapse show");
+  //     setAriaExpanded("true");
+  //     setNavbarToggler("navbar-toggler");
+     
+  //   } else {
+  //     setCollapse("navbar-collapse collapse");
+  //     setAriaExpanded("false");
+  //     setNavbarToggler("navbar-toggler collapsed");
+    
+  //   }
+  // }
+
   function clickToSearch() {
     if (collapseSearch === "navbar-collapse collapse") {
       setCollapseSearch("navbar-collapse collapse show");
@@ -40,7 +42,72 @@ function Header(props) {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light  justify-content-between">
+      <Navbar bg="light" expand="lg" className=" justify-content-between">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Brand
+            href="/"
+            className="d-flex align-items-center"
+            style={{ height: 45, width: 45 }}
+          >
+            <img
+              alt="/"
+              src={logo}
+              width="45"
+              height="45"
+              className="d-inline-block"
+            />
+          </Navbar.Brand>
+          <div className="d-flex justify-content-center align-items-center">
+            <FaSearch className="displayY fs-2" onClick={clickToSearch} />
+          </div>
+          <div className={collapseSearch}>
+            <ul className="displayY mx-auto m-5 mb-lg-0 align-items-center list-unstyled">
+              <li className="nav-item mx-auto">
+                <div className="searchbar">
+                  <input
+                    className="form-control searchinput"
+                    type="search"
+                    placeholder=""
+                    aria-label="Search"
+                  />
+                  <button className="btn searchbtn" type="submit">
+                    <FaSearch />
+                  </button>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto mx-auto align-items-center">
+              <Nav.Link href="/exhibition">
+                <h6 className="">展覽活動</h6>
+              </Nav.Link>
+              <Nav.Link href="/product">
+                <h6 className="">商品</h6>
+              </Nav.Link>
+              <Nav.Link to="/forum">
+                <h6 className="">討論區</h6>
+              </Nav.Link>
+
+              <li className="nav-item">
+                <div className="homeSearchBar justify-content-center align-items-stretch">
+                  <SearchBar />
+                  <Button variant="primary rounded-pill px-4">辦展覽</Button>
+                  <button className="align-items-center d-flex pb-2">
+                    <Link to="/forum">
+                      <FaUserCircle />
+                    </Link>
+                  </button>
+                </div>
+              </li>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* 耍笨版本 */}
+      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light  justify-content-between">
         <div className="container">
           <button
             className={navbarToggler}
@@ -58,10 +125,10 @@ function Header(props) {
           <Navbar.Brand
             href="/"
             className="d-flex align-items-center"
-            style={{ height: 45, width: 45}}
+            style={{ height: 45, width: 45 }}
           >
             <img
-              alt=""
+              alt="/"
               src={logo}
               width="45"
               height="45"
@@ -70,114 +137,48 @@ function Header(props) {
           </Navbar.Brand>
           <div className="d-flex justify-content-center align-items-center">
             <FaSearch className="displayY fs-2" onClick={clickToSearch} />
-          </div>
+          </div> */}
 
-          <div className={collapseSearch}>
-            <ul className="displayY mx-auto m-5 mb-lg-0 align-items-center list-unstyled">
-              <li className="nav-item">
-                <h6 className="">
-                  {" "}
-                  <div className="searchbar">
-                    <input
-                      className="form-control searchinput"
-                      type="search"
-                      placeholder=""
-                      aria-label="Search"
-                    />
-                    <button className="btn searchbtn" type="submit">
-                      <FaSearch />
-                    </button>
-                  </div>
-                </h6>
-              </li>
-            </ul>
-          </div>
+   
 
-          <div className={collapse} id="navbarTogglerDemo03">
-            <ul className="navbar-nav mx-auto mb-5 mb-lg-0 align-items-center">
+          {/* <div className={collapse} id="navbarTogglerDemo03">
+            <ul className="test navbar-nav mx-auto mb-5 mb-lg-0 align-items-center">
               <li className="nav-item">
-                {/* <a className="nav-link" aria-current="page" href="/exhibition"> */}
                 <Link to="/exhibition">
                   <h6 className="">展覽活動</h6>
                 </Link>
-                {/* </a> */}
               </li>
               <li className="nav-item">
-                {/* <a className="nav-link" href="/product"> */}
                 <Link to="/product">
                   <h6 className="Regular">商品</h6>
                 </Link>
-                {/* </a> */}
               </li>
 
               <li className="nav-item ">
-                {/* <a className="nav-link" href="/forum"> */}
                 <Link to="/forum">
                   <h6 className="Regular align-items-center">討論</h6>
                 </Link>
-                {/* </a> */}
               </li>
 
               <li className="nav-item">
                 <div className="homeSearchBar justify-content-center align-items-stretch">
                   <SearchBar />
-                  {/* <form className="d-flex">
-              <input type="search" placeholder="全站搜尋" />
-              <button type="submit">Search</button>
-            </form> */}
+                  <form className="d-flex">
+                    <input type="search" placeholder="全站搜尋" />
+                    <button type="submit">Search</button>
+                  </form>
                   <Button variant="primary rounded-pill px-4">辦展覽</Button>
-                  <button className="align-items-center d-flex pb-2"><Link to="/forum">
-                    <FaUserCircle /></Link>
+                  <button className="align-items-center d-flex pb-2">
+                    <Link to="/forum">
+                      <FaUserCircle />
+                    </Link>
                   </button>
                 </div>
               </li>
             </ul>
           </div>
         </div>
-      </nav>
-      {/* 忠憲寫的 */}
-      {/* <Navbar bg="light" className="text-center">
-        <Container style={{ width: 1440 }}>
-          <Navbar.Brand
-            href="/"
-            className="d-flex align-items-center"
-            style={{ height: 45, width: 45 }}
-          >
-            <img
-              alt=""
-              src={logo}
-              width="45"
-              height="45"
-              className="d-inline-block"
-            />
-          </Navbar.Brand>
-          <div
-            className="d-flex justify-content-center align-items-center"
-            style={{ gap: 99 }}
-          >
-            <Link to="/exhibition">
-              <h6 className="Regular">展覽活動</h6>
-            </Link>
-            <Link to="/product">
-              <h6 className="Regular">商品</h6>
-            </Link>
-            <Link to="/forum">
-              <h6 className="Regular">討論區</h6>
-            </Link>
-          </div>
-          <div className="homeSearchBar justify-content-center align-items-stretch">
-            <SearchBar />
-            <form className="d-flex">
-              <input type="search" placeholder="全站搜尋" />
-              <button type="submit">Search</button>
-            </form> 
-            <Button variant="primary rounded-pill px-4">辦展覽</Button>
-            <button className="d-flex">
-              <FaUserCircle />
-            </button>
-          </div>
-        </Container>
-      </Navbar> */}
+      </nav> */}
     </>
   );
 }
