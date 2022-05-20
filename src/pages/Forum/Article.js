d5907c9e051f77f8006fed7c87333f06d77e60c6
Penny <583forgame@gmail.com>
2022-05-20 23:12:42 +0800

    0520

--- src/pages/Forum/Article.js
+++ src/pages/Forum/Article.js
@@ -1,14 +1,105 @@
-//檢視文章
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FaStar,
+  FaAngleLeft,
+  FaAngleRight,
+  FaCommentDots,
+} from "react-icons/fa";
+import Comment from "./Comment";
+import Header from "../../component/Header";
+import Footer from "../../component/Footer";
+import "../../styles/AsideBar.scss";
+import "../../styles/global.scss";
+import "../../styles/Forum.scss";
 
 function Article(props) {
+  const content = `在有感謝順帶一讓我生活⋯九是我是太真的很你你會咖啡廳個，了嗎日向因為我的沒⋯次的邊還有為有沒有暴，跟而到最叫。是很想學住了一部分以沒有能，在一起這麼是心小心練不是：排球年輕女兒。一直到了。對方過了⋯不過我當更多，什麼會不會其他嗚嗚，就是我個決者的分開，買洗澡懷念不能接。會在他的想什麼都，我還道該怎舒適圈以後發現自己，得小的，之後恩我沒有⋯都是的本怎麼想當他不，黏土人。可能要記得但是，神有不做什麼。`;
   return (
     <>
-      <div>檢視文章</div>
-      <br />
+      <Header />
+      <div className="backBtn displayN">
+        <a href="/forum">
+          回討論區
+          <FaAngleLeft />
+        </a>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 liquidLeft"></div>
+          <div className="liquid col-lg-9 col-sm-12">
+            <section className="frContent mb-5">
+              <div className="articleHead d-flex align-items-start justify-content-between">
+                <div className="d-flex align-items-center">
+                  <img
+                    className="avatar m-4"
+                    src="https://picsum.photos/200"
+                    alt="userpicture"
+                  />
+                  <div>
+                    <div className="p txtGray">作者</div>
+                    <div className="h4 Regular">標題標題標題標題</div>
+                  </div>
+                </div>
+                <div className="like d-flex justify-content-center align-items-center">
+                  <FaStar className="fs-5" />
+                  <div className="px-2">收藏</div>
+                </div>
+              </div>
+              <div className="articleBody">
+                <p>{content}</p>
+              </div>
+              <div className="articleFoot">
+                <div>
+                  <div className="d-flex justify-content-between">
+                    <p className="category">分類：{"心得分享"}</p>
+                    <div className="d-flex align-items-center">
+                      <FaCommentDots />
+                      <div className="px-2">25</div>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="d-flex justify-content-between">
+                    <div className="d-flex align-items-center">
+                      <FaAngleLeft />
+                      <div>上一篇文章</div>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <FaAngleRight />
+                      <div>下一篇文章</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* 新增回復 */}
+              <section className="d-flex flex-column border my-5 p-4 newComment">
+                <div className="d-flex align-items-center">
+                  <img
+                    className="avatar m-3"
+                    src="https://picsum.photos/200"
+                    alt="userpicture"
+                  />
+                  <div>Me</div>
+                </div>
+                <input
+                  id="inputBox"
+                  type="text"
+                  className="my-3 form-control"
+                ></input>
+                <div className="align-self-end">
+                  <button className="btn btn-primary rounded-pill">
+                    新增回覆
+                  </button>
+                </div>
+              </section>
+              <Comment />
+            </section>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Article
+export default Article;
