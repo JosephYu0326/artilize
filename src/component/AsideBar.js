d5e19b64b07d51f82fec2dba92a1d5ac7b1a249a
58peiShan <99083443+58peiShan@users.noreply.github.com>
2022-05-20 18:11:03 +0800

    Update AsideBar.js

--- src/component/AsideBar.js
+++ src/component/AsideBar.js
@@ -1,8 +1,39 @@
+import { logDOM } from "@testing-library/react";
 import React from "react";
 import { FaAngleRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import "../styles/AsideBar.scss";
 
 function AsideBar(props) {
+  //在要引入的<AsideBar / >裡面，加上btn屬性接收參數
+  //<AsideBar btn={常數名稱}
+  //const 常數名稱 = {
+    //   btnTitle: [
+    //     "會員資料",
+    //     "訂單紀錄",
+    //     "文章收藏",
+    //     "商品收藏",
+    //     "個人主頁",
+    //     "我的優惠券",
+    //   ],
+    //   btnTo: [
+    //     "/users",
+    //     "/users/orderrecord",
+    //     "/users/articlecollect",
+    //     "/users/productcollect",
+    //     "/users/personalpage",
+    //     "/users/mycoupon"
+    //   ],
+    // }
+  const btnTitleName = props.btn.btnTitle;
+  const btnRouterName = props.btn.btnTo;
+
+  const btnList = btnTitleName.map((v, i) => { 
+    return (
+      <Link key={i} to={btnRouterName[i]} className="btn asideBtn">{btnTitleName[i]}</Link>
+    );
+  });
+
   return (
     <>
       <input type="checkbox" name="" id="sideMenu-switch"></input>
@@ -11,15 +42,12 @@ function AsideBar(props) {
           htmlFor="sideMenu-switch"
           className="align-self-end justify-content-center"
         >
-          <FaAngleRight className="faAngleRight" />
+          <FaAngleRight className="faAngleRight " />
         </label>
-        <button className="btn asideBtn">{props.btn1}</button>
-        <button className="btn asideBtn">{props.btn2}</button>
-        <button className="btn asideBtn">{props.btn3}</button>
-        <button className="btn asideBtn">{props.btn4}</button>
-        <button className="btn asideBtn">{props.btn5}</button>
-        <button className="btn asideBtn">{props.btn6}</button>
+        {btnList}
       </aside>
+
+    
     </>
   );
 }
