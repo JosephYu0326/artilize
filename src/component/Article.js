e16faec79db765e6cbb76f74d7f1e68630a4fa35
Penny <583forgame@gmail.com>
2022-05-22 14:33:09 +0800

    0521

--- src/component/Article.js
+++ src/component/Article.js
@@ -2,10 +2,25 @@ import React from 'react';
 import { FaStar, FaCommentDots } from "react-icons/fa";
 
 function Article(props) {
+    const isSignIn = true
     const articleDetails = props.articDetails
-    function which(e) {
+    // function which(e) {
+    //     console.log(e.target);
+    // }
+
+    function getPerArticle(e){
         console.log(e.target);
     }
+
+    function handleLike() {
+        if(isSignIn){
+            alert("收藏文章")
+        }else{
+            alert("請先登入！")
+        }
+    }
+
+
     const article = articleDetails.map((v, i) => {
         return (
             <div key={i}>
@@ -19,8 +34,8 @@ function Article(props) {
                             />
                             <p className="pBig text-center">{articleDetails[i].author}</p>
                         </div>
-                        <h5 className="h5 ExtraBold mx-auto ms-2 mb-0 title" onClick={which}>
-                            {articleDetails[i].title}
+                        <h5 className="h5 ExtraBold mx-auto ms-2 mb-0 title">
+                            <Link to="/forum/article/id=2}">{articleDetails[i].title}</Link>
                         </h5>
                     </div>
                     <div>
@@ -33,7 +48,7 @@ function Article(props) {
                 <div className="social d-flex justify-content-end align-items-center">
                     <FaCommentDots />
                     <div className="p-2">{articleDetails[i].comment}</div>
-                    <FaStar />
+                    <FaStar onClick={handleLike} />
                 </div>
                 <hr />
             </div>
