e14e0f67593c6c021f5e4f6b088f0c3e051268c1
Penny <583forgame@gmail.com>
2022-05-24 22:40:51 +0800

    修正新增文章選擇類別的問題

--- src/pages/Forum/AddArticle.js
+++ src/pages/Forum/AddArticle.js
@@ -1,6 +1,7 @@
 //新增文章
 import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import tinymce from 'tinymce/tinymce'
 import { Editor } from '@tinymce/tinymce-react'
 import { FaAngleLeft } from 'react-icons/fa'
 import { Form, Button } from 'react-bootstrap'
@@ -8,22 +9,58 @@ import Header from '../../component/Header'
 import Footer from '../../component/Footer'
 
 function AddArticle(props) {
+  // ========tinyMCE東東======
+  // const editorRef = useRef(null)
+  // const log = () => {
+  //   if (editorRef.current) {
+  //     console.log(editorRef.current.getContent())
+  //   }}
+  // ========tinyMCE東東======
+
+  const created_time = new Date()
   const [category, setCategory] = useState([])
-  const editorRef = useRef(null)
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent())
-    }
-  }
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [ChioseCategory, setChioseCategory] = useState("")
+  console.log(title);
+  console.log(content);
+
   const categoryChoice = category.map((v, i) => {
     return (
       <>
-        <option key={i} value={i}>
+        <option key={i} value={category[i].sn}>
           {category[i].thema}
         </option>
       </>
     )
   })
+
+  console.log(ChioseCategory);
+
+  // --------
+  let headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  }
+
+  let body = {
+    "title": title,
+    "content": content,
+    "category": 2,
+  }
+  console.log(created_time);
+  function handleClick(e) {
+    e.preventDefault()
+    fetch(`${process.env.REACT_APP_API_URL}/forum/addarticle`, {
+      method: "POST",
+      headers: headers,
+      //別忘了把主體参數轉成字串，否則資料會變成[object Object]，它無法被成功儲存在後台
+      body: JSON.stringify(body)
+    })
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+  // --------
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/forum/category`)
@@ -32,6 +69,7 @@ function AddArticle(props) {
         setCategory(data)
       })
   }, [])
+
 
   return (
     <>
@@ -45,14 +83,28 @@ function AddArticle(props) {
       <div className="container">
         <div className="frContent">
           <h3>新增討論</h3>
-          <form action="">
+          <form name='articleForm'>
             <Form.Control
+              value={title}
+              name="title"
               className="my-5"
               size="lg"
               type="text"
-              placeholder="請輸入標題"
+              onChange={(e) => setTitle(e.target.value)}
             />
-            <Editor
+            <Form.Control
+              value={content}
+              form='articleForm'
+              neme="content"
+              style={{ height: '300px' }}
+              as='textarea'
+              onChange={(e) => setContent(e.target.value)}
+            />
+            {/* ========tinyMCE東東====== */}
+            {/* <Editor
+              id='inputContent'
+              //onEditorChange={(e) => setContent(e.target.value)}
+              value={content}
               apiKey="lx36ygwprus1n4asjcpxlw72wjnkjsm90cs4xl68rlcj2qff"
               onInit={(evt, editor) => (editorRef.current = editor)}
               initialValue="<p>請輸入內文</p>"
@@ -85,25 +137,19 @@ function AddArticle(props) {
                 content_style:
                   "body { font-family: 'Noto Sans TC', sans-serif; font-size:14px }",
               }}
-            />
+            /> */}
             <div className="container">
               <div className="row mx-auto">
-                <Form.Select className="mt-5" aria-label="">
+                <Form.Select id='pp' className="mt-5" aria-label="" onChange={(e) => setChioseCategory(e.target.value)}>
                   <option>文章分類</option>
                   {categoryChoice}
                 </Form.Select>
-
-                {/* <Button variant="secondary col-sm-12 col-md-5 rounded-pill my-5 mx-auto">
-                  儲存草稿
-                </Button> */}
-                <Button
-                  type="submit"
-                  variant="primary col-sm-12 col-md-5 rounded-pill my-5 mx-auto"
-                >
+                <Button onClick={handleClick} variant="primary col-sm-12 col-md-5 rounded-pill my-5 mx-auto">
                   新增討論
                 </Button>
               </div>
             </div>
+            {/* ========tinyMCE東東====== */}
             {/* <button onClick={log}>Log editor content</button> */}
           </form>
         </div>
