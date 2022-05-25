5ca284e4f6959c3e245e06d94cddc6e18ecfb845
Penny <583forgame@gmail.com>
2022-05-25 13:32:56 +0800

    上下一篇文章連結

--- src/pages/Forum/Article.js
+++ src/pages/Forum/Article.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useHistory } from 'react-router-dom'
 import FadeIn from 'react-fade-in/lib/FadeIn'
 import {
   FaStar,
@@ -14,12 +14,16 @@ import '../../styles/AsideBar.scss'
 import '../../styles/global.scss'
 import '../../styles/Forum.scss'
 
+
 function Article(props) {
   const [perArticle, setPerArticle] = useState([])
   const [preArticle, setPreArticle] = useState([])
   const [nextArticle, setNextArticle] = useState([])
   const { forumid } = useParams()
-
+  const history = useHistory()
+  const goBack = () => {
+    history.goBack()
+  }
   // useEffect(() => {
   //   fetch(`${process.env.REACT_APP_API_URL}/forum`)
   //     .then((res) => res.json())
@@ -28,49 +32,69 @@ function Article(props) {
   //     })
   // }, [])
 
-
-
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/forum/${forumid}`)
       .then((res) => res.json())
       .then((data) => {
-        setPerArticle(data[1])
-        setPreArticle(data[0])
-        setNextArticle(data[2])
-        console.log(data)
-      })
+        if (data.length == 3) {
+          setPreArticle(data[0])
+          setPerArticle(data[1])
+          setNextArticle(data[2])
+          console.log(data);
+        } else {
+          setPreArticle(data[0])
+          setPerArticle(data[1])
+          setNextArticle({ title: '', created_time: '', content: '' })
+        }
+      }
+      )
   }, [])
 
   function next() {
-    // fetch(`${process.env.REACT_APP_API_URL}/forum/${nextArticle.article_id}`)
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     setPerArticle(data[1])
-    //     setPreArticle(data[0])
-    //     setNextArticle(data[2])
-    console.log(nextArticle.title)
-    // })
+    fetch(`${process.env.REACT_APP_API_URL}/forum/${nextArticle.article_id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length == 3) {
+          setPreArticle(data[0])
+          setPerArticle(data[1])
+          setNextArticle(data[2])
+          console.log(data);
+        } else {
+          setPreArticle(data[0])
+          setPerArticle(data[1])
+          setNextArticle({ title: '', created_time: '', content: '' })
+        }
+        console.log(nextArticle.title)
+      })
   }
 
   function pre() {
-    // fetch(`${process.env.REACT_APP_API_URL}/forum/${preArticle.article_id}`)
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     setPerArticle(data[1])
-    //     setPreArticle(data[0])
-    //     setNextArticle(data[2])
-    console.log(preArticle.title)
-    // })
+    fetch(`${process.env.REACT_APP_API_URL}/forum/${preArticle.article_id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length == 3) {
+          setPreArticle(data[0])
+          setPerArticle(data[1])
+          setNextArticle(data[2])
+          console.log(data);
+        } else {
+          setPreArticle(data[0])
+          setPerArticle(data[1])
+          setNextArticle({ title: '', created_time: '', content: '' })
+        }
+        console.log(preArticle.title)
+      })
   }
+
+
+
 
   return (
     <>
       <Header />
-      <div className="backBtn displayN">
-        <Link to="/forum">
-          回討論區
-          <FaAngleLeft />
-        </Link>
+      <div className="backBtn displayN" onClick={goBack}>
+        回討論區
+        <FaAngleLeft />
       </div>
       <div className="container">
         <FadeIn className="row">
@@ -79,13 +103,17 @@ function Article(props) {
             <section className="frContent mb-5">
               <div className="articleHead d-flex align-items-start justify-content-between">
                 <div className="d-flex align-items-center">
-                  <img
-                    className="avatar m-4"
-                    src="https://picsum.photos/200"
-                    alt="userpicture"
-                  />
+                  <Link to={`/forum/FrPersonalPage/${perArticle.users_id}`}>
+                    <img
+                      className="avatar m-4"
+                      src="https://picsum.photos/200"
+                      alt="userpicture"
+                    />
+                  </Link>
                   <div>
-                    <div className="p txtGray">{perArticle.nickname}</div>
+                    <Link className="p txtGray" to={`/forum/FrPersonalPage/${perArticle.users_id}`}>
+                      {perArticle.nickname}
+                    </Link>
                     <div className="h4 ps-3 Regular">{perArticle.title}</div>
                   </div>
                 </div>
