4299cebb8e7f54e3984ed51c766c4155217d99c0
孤鴻寄語默蒼離 <101337985+Chuan-OWO@users.noreply.github.com>
2022-05-27 15:33:12 +0800

    新增展覽B2B列表相關

--- src/pages/B2B/B2B.js
+++ src/pages/B2B/B2B.js
@@ -13,7 +13,51 @@ import Col from 'react-bootstrap/Col'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 
+import '../../styles/B2BListe.scss'
+
+import { abilityEx } from './AbilityEx'
+
+const extendAbilityEx = (array) => {
+  return array.map((v, i) => {
+    let expire = true
+
+    if (+new Date(v.endtime) > +new Date()) {
+      console.log(+new Date())
+      expire = false
+    }
+
+    return { ...v, expire }
+  })
+}
+
 function B2B(props) {
+  const [data, setDate] = useState(extendAbilityEx(abilityEx))
+  const [key, setKey] = useState('all')
+
+  console.log(extendAbilityEx(abilityEx))
+
+  // 設定展覽數
+  const setExCount = (newCount, i) => {
+    const newExhibitionInorder = [...abilityEx]
+    newExhibitionInorder[i].count = newCount < 1 ? 1 : newCount
+    setDate(newExhibitionInorder)
+  }
+  // 展覽總數
+  // const totalNumberEx = () => {
+  //   let total = 0
+  //   for (let i = 0; i < exhibitionInorder.length; i++) {
+  //     total += 1
+  //   }
+  //   return total
+  // }
+  // 展覽刪除
+  const handleDeleteEx = (id) => {
+    alert('確定要刪除該筆資料嗎？')
+    const newExhibitionInorder = [...abilityEx].filter((v, i) => {
+      return v.id !== id
+    })
+    setDate(newExhibitionInorder)
+  }
   return (
     <>
       <div>廠商後臺</div>
@@ -25,16 +69,96 @@ function B2B(props) {
         <Row>
           <Col>
             <Tabs
-              defaultActiveKey="profile"
+              //defaultActiveKey="profile"
               id="uncontrolled-tab-example"
               className="mb-3"
+              activeKey={key}
+              onSelect={(k) => setKey(k)}
             >
+              {/* title={`已發布(${''})` */}
               <Tab eventKey="start" title={`已發布(${''})`}>
                 <B2Btotal />
-                <B2Bliste />
+                <div className="exhibitionInorder">
+                  {data
+                    .filter((v, i) => {
+                      return v.expire === false
+                    })
+                    .map((v, i) => {
+                      console.log(v)
+                      console.log(v, i)
+                      return (
+                        <B2Bliste
+                          key={v.id}
+                          id={v.id}
+                          title={v.title}
+                          startime={v.startime}
+                          endtime={v.endtime}
+                          createtime={v.createtime}
+                          image={v.image}
+                          count={v.count}
+                          handleDelete={() => {
+                            handleDeleteEx(v.id)
+                          }}
+                          setExCount={(newCount) => {
+                            setExCount(newCount, i)
+                          }}
+                        />
+                      )
+                    })}
+                </div>
               </Tab>
-              <Tab eventKey="end" title={`已結束(${''})`}></Tab>
-              <Tab eventKey="all" title={`全部(${''})`}></Tab>
+              <Tab eventKey="end" title={`已結束(${''})`}>
+                <div className="exhibitionInorder">
+                  {data
+                    .filter((v, i) => {
+                      return v.expire === true
+                    })
+                    .map((v, i) => {
+                      return (
+                        <B2Bliste
+                          key={v.id}
+                          id={v.id}
+                          title={v.title}
+                          startime={v.startime}
+                          endtime={v.endtime}
+                          createtime={v.createtime}
+                          image={v.image}
+                          count={v.count}
+                          handleDelete={() => {
+                            handleDeleteEx(v.id)
+                          }}
+                          setExCount={(newCount) => {
+                            setExCount(newCount, i)
+                          }}
+                        />
+                      )
+                    })}
+                </div>
+              </Tab>
+              <Tab eventKey="all" title={`全部(${''})`}>
+                <div className="exhibitionInorder">
+                  {data.map((v, i) => {
+                    return (
+                      <B2Bliste
+                        key={v.id}
+                        id={v.id}
+                        title={v.title}
+                        startime={v.startime}
+                        endtime={v.endtime}
+                        createtime={v.createtime}
+                        image={v.image}
+                        count={v.count}
+                        handleDelete={() => {
+                          handleDeleteEx(v.id)
+                        }}
+                        setExCount={(newCount) => {
+                          setExCount(newCount, i)
+                        }}
+                      />
+                    )
+                  })}
+                </div>
+              </Tab>
             </Tabs>
           </Col>
         </Row>
--- src/pages/B2B/B2Bliste.js
+++ src/pages/B2B/B2Bliste.js
@@ -1,43 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 function B2Bliste(props) {
-  //   const { id, title, image, startime, endtime } = props
+  //   const { id, title, image, startime, endtime,createtime } = props
   //   const { image } = props
+  const { id, title, image, startime, endtime, createtime, handleDelete } =
+    props
   return (
     <>
-      <div className="row d-flex align-items-center">
+      <div className="row d-flex align-items-center mb-5 border">
         <div className="col-md-2 style={{border:'1px solid'}}">123456789</div>
-        <div className="col-md-3 col-sm-12">123456789</div>
         <div className="col-md-3 col-sm-12">
-          <label>123456789</label>
-          <label>123456789</label>
-          <label>123456789</label>
+          {' '}
+          <img
+            src={require(`${image}`)}
+            className="imgListe"
+            alt="exhibitionImg"
+          />
         </div>
-        <div className="col-md-2 col-sm-12">123456789</div>
-        <div className="col-md-2 col-sm-12">123456789</div>
-      </div>
-      <div className="row d-flex align-items-center">
-        <div className="col-2 style={{border:'1px solid'}}">123456789</div>
-        <div className="col-3">123456789</div>
-        <div className="col-3">
-          <label>123456789</label>
-          <label>123456789</label>
-          <label>123456789</label>
+        <div className="col-md-3 col-sm-12">
+          <div>{title}</div>
+          <div>開始時間:{startime}</div>
+          <div>結束時間:{endtime}</div>
+          <div>建立時間:{createtime}</div>
         </div>
-        <div className="col-2">123456789</div>
-        <div className="col-2">123456789</div>
-      </div>
-      <div className="row d-flex align-items-center">
-        <div className="col-2 style={{border:'1px solid'}}">123456789</div>
-        <div className="col-3">123456789</div>
-        <div className="col-3">
-          <label>123456789</label>
-          <label>123456789</label>
-          <label>123456789</label>
+        <div className="col-md-2 col-sm-12">XXX</div>
+        <div className="col-md-2 col-sm-12">
+          <button className="trash-btn">
+            {/* <FontAwesomeIcon
+              className="trash"
+              icon={faTrashAlt}
+              onClick={handleDelete}
+            /> */}
+          </button>
         </div>
-        <div className="col-2">123456789</div>
-        <div className="col-2">123456789</div>
       </div>
     </>
   )
