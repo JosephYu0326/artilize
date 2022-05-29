3537dccbc8085c471458fabc63ae1b9db4250cdc
Penny <583forgame@gmail.com>
2022-05-29 23:13:03 +0800

    跳轉置頂

---
+++ src/component/ScrollToTop.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop = props => {
+
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+
+        window.scrollTo(0, 0)
+
+    }, [pathname]);
+
+    return props.children;
+
+}
+
+export default ScrollToTop
