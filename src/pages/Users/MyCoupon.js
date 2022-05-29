13dd555eae3fd19e607cdc716a470ad7ac30e63d
Penny <583forgame@gmail.com>
2022-05-29 23:14:12 +0800

    Fade in

--- src/pages/Users/MyCoupon.js
+++ src/pages/Users/MyCoupon.js
@@ -36,46 +36,49 @@ function MyCoupon(props) {
     <>
       <Header />
       <AsideBar btn={btnList} />
+      <FadeIn>
 
-      <div className="container ">
-        <div className="userPage mx-auto frContent row justify-content-between">
-          <h3>我的優惠券</h3>
 
-          <div className="coupon row col-md-12 col-lg-6 my-2">
-            <div className="couponImage col-4">
-              {/* <img src={couponBG} /> */}
-              <FaTicketAlt className="couponIcon mt-2" />
-              <h5>{ticketList.exhibition[0]}</h5>
-            </div>
-            <div className="couponMsg text-center col-7">
-              <h5>憑此券可{ticketList.exhibition[1]}</h5>
-              <p className="pRegular align-self-start">
-                {ticketList.exhibition[2]}
-              </p>
-              <div className="d-flex justify-content-around">
-                <p className="pRegular">期限{ticketList.exhibition[3]}</p>
-                <p className="pRegular">{ticketList.exhibition[4]}</p>
+        <div className="container ">
+          <div className="userPage  mx-auto frContent row justify-content-between">
+            <h3>我的優惠券</h3>
+
+            <div className="coupon row col-md-12 col-lg-6 my-2">
+              <div className="couponImage col-4">
+                {/* <img src={couponBG} /> */}
+                <FaTicketAlt className="couponIcon mt-2" />
+                <h5>{ticketList.exhibition[0]}</h5>
+              </div>
+              <div className="couponMsg text-center col-7">
+                <h5>憑此券可{ticketList.exhibition[1]}</h5>
+                <p className="pRegular align-self-start">
+                  {ticketList.exhibition[2]}
+                </p>
+                <div className="d-flex justify-content-around">
+                  <p className="pRegular">期限{ticketList.exhibition[3]}</p>
+                  <p className="pRegular">{ticketList.exhibition[4]}</p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="coupon row col-md-12 col-lg-6 my-2">
-            <div className="couponImage col-4">
-              {/* <img src={couponBG} /> */}
-              <FaTicketAlt className="couponIcon mt-2" />
-              <h5>{ticketList.product[0]}</h5>
-            </div>
-            <div className="couponMsg text-center col-7">
-              <h5>憑此券可{ticketList.product[1]}</h5>
-              <p className="pRegular">{ticketList.product[2]}</p>
-              <div className="d-flex justify-content-around">
-                <p className="pRegular">期限{ticketList.product[3]}</p>
-                <p className="pRegular">{ticketList.product[4]}</p>
+            <div className="coupon row col-md-12 col-lg-6 my-2">
+              <div className="couponImage col-4">
+                {/* <img src={couponBG} /> */}
+                <FaTicketAlt className="couponIcon mt-2" />
+                <h5>{ticketList.product[0]}</h5>
+              </div>
+              <div className="couponMsg text-center col-7">
+                <h5>憑此券可{ticketList.product[1]}</h5>
+                <p className="pRegular">{ticketList.product[2]}</p>
+                <div className="d-flex justify-content-around">
+                  <p className="pRegular">期限{ticketList.product[3]}</p>
+                  <p className="pRegular">{ticketList.product[4]}</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </FadeIn>
       <Footer />
     </>
   );
