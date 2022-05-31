0372ddf3ccceafd67727e431441f2c20e0aa84bf
Penny <583forgame@gmail.com>
2022-05-31 22:56:51 +0800

    收藏文章更改

--- src/component/Article.js
+++ src/component/Article.js
@@ -1,53 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import { Alert } from 'react-bootstrap'
-import { FaStar, FaCommentDots } from 'react-icons/fa'
+import React from 'react'
+import { FaStar } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import FadeIn from 'react-fade-in/lib/FadeIn'
+import CommentCount from './CommentCount'
 function Article(props) {
-    const isSignIn = false
     const articleDetails = props.articDetails
     console.log(articleDetails)
 
-
-
-    function handleLike() {
-        if (isSignIn) {
-            alert('收藏文章')
-        } else {
-            <Alert variant="primary">
-                "FU"
-            </Alert>
-        }
-    }
-    const [temp, setTemp] = useState([])
-
-    // console.log(temp)
-    // const CallComment = () => {
-    //     fetch(`${process.env.REACT_APP_API_URL}/forum/comments/${temp}`)
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             setComments(data)
-    //             console.log(temp);
-    //             console.log("留言:");
-    //             console.log(comments);
-    //         })
-    //     return (<>
-    //         <div>{comments.length}</div>
-    //     </>)
-    // }
-
-    // useEffect(() => {
-    //     CallComment()
-    // }, [])
-
-
-    // ===========
     const article = articleDetails.map((v, i) => {
-        // const temp = articleDetails[i].created_time.slice(0, 10)
-        // console.log(temp)
-        // ======留言匯入
-        // setTemp(articleDetails[i].article_id)
+
         return (
-            <div key={i}>
+            <FadeIn key={i}>
                 <div className="perContentHead my-4 d-flex justify-content-between">
                     <div className="text-start d-flex align-items-center ">
                         <div>
@@ -68,20 +31,18 @@ function Article(props) {
                     </div>
                 </div>
                 <div className="preViewContent pb-3" dangerouslySetInnerHTML={{ __html: `${articleDetails[i].content}` }}></div>
-                {/* <pre className="preViewContent pb-3">{articleDetails[i].content}</pre> */}
                 <div className="social d-flex justify-content-between align-items-center">
                     <time>{articleDetails[i].created_time}</time>
                     <div className='d-flex align-items-center'>
-                        <FaCommentDots />
+                        <CommentCount num={articleDetails[i].article_id} />
                         <div className="p-2">
-                            {/* <Comment id={articleDetails[i]} /> */}
                         </div>
-                        <FaStar onClick={handleLike} />
+                        <FaStar className={`${articleDetails[i].favorited ? 'likeIt' : ''}`} />
 
                     </div>
                 </div>
                 <hr />
-            </div>
+            </FadeIn>
         )
     })
     return <>{article}</>
