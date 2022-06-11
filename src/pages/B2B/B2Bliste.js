c81673e0b6cc848f05d4ebdcaf6072143149170e
孤鴻寄語默蒼離 <101337985+Chuan-OWO@users.noreply.github.com>
2022-06-11 19:03:03 +0800

    亂改欄位樣式

--- src/pages/B2B/B2Bliste.js
+++ src/pages/B2B/B2Bliste.js
@@ -8,28 +8,33 @@ function B2Bliste(props) {
     props
   return (
     <>
-      <div className="row d-flex align-items-center mb-5 border">
+      <div className="row d-flex align-items-center mb-5 border bobo">
         <div className="col-md-2 style={{border:'1px solid'}}"></div>
-        <div className="col-md-3 col-sm-12">
+        <div className="col-md-3 col-sm-12 my-3">
           <img
             src={`http://localhost:5000/uploads/${pic1}`}
-            className="imgListe"
+            className="imgListe rounded-3 "
             alt="exhibitionImg"
           />
         </div>
-        <div className="col-md-3 col-sm-12">
-          <div className="text-truncate">活動名稱:{aName}</div>
+        <div className="col-md-3 col-sm-12 my-3">
+          <div className="text-truncate ">活動名稱:{aName}</div>
           <div>開始時間:{start}</div>
           <div>結束時間:{end}</div>
           <div>建立時間:{time}</div>
         </div>
-        <div className="col-md-2 col-sm-12">
-          <button className="trash-btn">
-            <Link to={`/b2b/edit/${handleRRR}`}>編輯</Link>
+        <div className="col-md-2 col-sm-12 my-3 d-grid gap-2 mx-auto">
+          <button className="trash-btn btn btn-primary ">
+            <Link
+              className="link-light text-decoration-none "
+              to={`/b2b/edit/${handleRRR}`}
+            >
+              編輯
+            </Link>
           </button>
         </div>
-        <div className="col-md-2 col-sm-12">
-          <button className="trash-btn" onClick={handleDelete}>
+        <div className="col-md-2 col-sm-12 my-3 d-grid gap-2  mx-auto">
+          <button className="trash-btn btn btn-danger " onClick={handleDelete}>
             刪除
           </button>
           {/* <FontAwesomeIcon
